e6095ece710b97744c0457ee3408bdb45a2cecbb
yousseflearn <youssefeasylearn@gmail.com>
2024-12-06 21:17:29 +0400

    Creation Services Section

---
+++ components/Home/Services/Services.tsx
@@ -0,0 +1,24 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
+import { servicesData } from '@/data/data';
+import React from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+  return (
+    <div className="pt-16 pb-16 bg-[#0f0715]">
+      <SectionHeading>Services</SectionHeading>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-[80%] mx-auto items-center mt-20">
+        {servicesData.map((service) => {
+          return (
+            <div key={service.id}>
+              {/* ServiceCard */}
+              <ServiceCard service={service} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
